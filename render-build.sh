3edec9a2bdea027a09a3ee23cf5ec1174894ded5
Ferpoks <148285162+Ferpoks@users.noreply.github.com>
2025-08-22 15:18:21 +0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 set -e
-pip install --upgrade pip
+
+# (اختياري) تثبيت حزم APT إن كنت تستخدم apt.txt
+if [ -f apt.txt ]; then
+  while read -r pkg; do
+    sudo apt-get update && sudo apt-get install -y "$pkg"
+  done < apt.txt
+fi
+
+# تثبيت بايثون باكدجات
+pip install -U pip
 pip install -r requirements.txt
