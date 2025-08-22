35e55195c46965d73233ff83555d7236f782cf82
Ferpoks <148285162+Ferpoks@users.noreply.github.com>
2025-08-22 16:03:34 +0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-# (اختياري) تثبيت حزم APT إن وُجد ملف apt.txt
-if [[ -f apt.txt ]]; then
-  while read -r pkg; do
-    sudo apt-get update && sudo apt-get install -y "$pkg"
-  done < apt.txt
-fi
-
-# تثبيت pip ثم المتطلبات
 pip install -U pip
 pip install -r requirements.txt
